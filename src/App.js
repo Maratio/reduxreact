f6c10cda745214111e88b897c2110d5f25d51945
Maratio <marat.kmr@yandex.ru>
2025-02-03 23:02:39 +0300

    firstConfiqurationRedux_(store,reducer,dispatch, state, provider)

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector((state) => state.cash)
+  console.log(cash);
+
+
+  const addCash = (newcash) => {
+    dispatch({ type: 'ADD_CASH', payload: newcash })
+  }
+
+  const getCash = (newcash) => {
+    dispatch({ type: 'GET_CASH', payload: newcash })
+  }
+
+  return (
+    <div>
+      <div style={{
+        fontSize: '3rem'
+      }}>{cash}</div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button
+          onClick={() => addCash(Number(prompt()))}
+          style={{ width: 'auto', height: 30, background: 'green', margin: 20 }}>Пополнить счет</button>
+        <button
+          onClick={() => getCash(Number(prompt()))}
+          style={{ width: 'auto', height: 30, background: 'green', margin: 20 }}>Снять со счета</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
